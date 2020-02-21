2e86d5be77c79e3d019863c5ee97d0f70c5b56db
Mehran Prs <mehr.prs@gmail.com>
2020-02-21 20:34:56 +0330

    Refactor mgm entity name from KittyModel to Entity

--- entityadapter/mgmentity/model.go
+++ entityadapter/mgmentity/model.go
@@ -1,18 +1,18 @@
 package mgmentity
 
-// KittyModel struct contain model's default fields.
-type KittyModel struct {
+// Entity struct contain model's default fields.
+type Entity struct {
 	IDField    `bson:",inline"`
 	DateFields `bson:",inline"`
 }
 
 // Creating function call to it's inner fields defined hooks
-func (model *KittyModel) Creating() error {
+func (model *Entity) Creating() error {
 	return model.DateFields.Creating()
 }
 
 // Saving function call to it's inner fields defined hooks
-func (model *KittyModel) Saving() error {
+func (model *Entity) Saving() error {
 	return model.DateFields.Saving()
 }
 
