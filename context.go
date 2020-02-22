082936c63ce210627cb0596f7fa9b6a4a4628530
Mehran Prs <mehr.prs@gmail.com>
2020-02-22 19:02:33 +0330

    Add context implementation.

--- context.go
+++ context.go
@@ -1,11 +1,59 @@
 package kitty
 
-import "context"
+import (
+	"context"
+)
 
+// Context is the kitty context to use in services.
 type Context interface {
 	context.Context
 
+	// RequestID returns the request id.
+	RequestID() string
+	// User returns the user
 	User() User
-	Translator() Translator
+
+	// Log returns the kitty logger customized for specific request.
 	Log() Logger
+
+	// Translator returns the translator localized relative to the users request.
+	Translator() Translator
 }
+
+type defaultContext struct {
+	context.Context
+	requestID  string
+	user       User
+	logger     Logger
+	translator Translator
+}
+
+func (c defaultContext) RequestID() string {
+	return c.requestID
+}
+
+func (c defaultContext) User() User {
+	return c.user
+}
+
+func (c defaultContext) Log() Logger {
+	return c.logger
+}
+
+func (c defaultContext) Translator() Translator {
+	return c.translator
+}
+
+// NewCtx returns new kitty Context.
+func NewCtx(requestID string, user User, logger Logger, translator Translator) Context {
+	return &defaultContext{
+		Context:    context.Background(),
+		requestID:  requestID,
+		user:       user,
+		logger:     logger,
+		translator: translator,
+	}
+}
+
+// Assert defaultContext implements the kitty Context.
+var _ Context = &defaultContext{}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/Kamva/gutil v0.0.0-20200220090603-f3d8cdcee96d
 	github.com/Kamva/mgm/v2 v2.0.0 // indirect
+	github.com/labstack/echo/v4 v4.1.14 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.0.3
 	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/spf13/viper v1.6.2 // indirect
