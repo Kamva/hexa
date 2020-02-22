089fe1988fbbf72a16516bad581cdff6c7baf0c0
Mehran Prs <mehr.prs@gmail.com>
2020-02-22 17:09:31 +0330

    Add GetUsername to the User interface.

--- User.go
+++ User.go
@@ -1,6 +1,12 @@
 package kitty
 
 type User interface {
+	// Specify that user is guest or nop.
 	IsGuest() bool
+
+	// Return users identifier (if guest return just empty string or something like this.)
 	GetID() interface{}
+
+	// Username can be unique username,email,phone number or everything else that can use as username.
+	GetUsername() string
 }
