6091eaa7a6f734d555e655c70756b098e0d55f1d
Mehran Prs <mehr.prs@gmail.com>
2020-02-22 21:27:32 +0330

    Add guest user.

--- User.go
+++ User.go
@@ -1,12 +1,33 @@
 package kitty
 
 type User interface {
-	// Specify that user is guest or nop.
+	// Specify that user is guestUser or no.
 	IsGuest() bool
 
-	// Return users identifier (if guest return just empty string or something like this.)
+	// Return users identifier (if guestUser return just empty string or something like this.)
 	GetID() interface{}
 
 	// Username can be unique username,email,phone number or everything else that can use as username.
 	GetUsername() string
 }
+
+type guestUser struct {
+}
+
+func (g guestUser) IsGuest() bool {
+	return true
+}
+
+func (g guestUser) GetID() interface{} {
+	return "__guest_id__"
+}
+
+func (g guestUser) GetUsername() string {
+	return "__guest__username__"
+}
+
+func NewGuestUser() User {
+	return guestUser{}
+}
+
+var _ User = &guestUser{}
