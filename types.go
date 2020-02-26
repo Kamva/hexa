6cd9e9a1ed7754f0f55b6255d0899f184c080015
Mehran Prs <mehr.prs@gmail.com>
2020-02-26 12:27:12 +0330

    Update password type marshaller

--- types.go
+++ types.go
@@ -1,9 +1,14 @@
 package kitty
 
+import "encoding/json"
+
 type Secret string
 
-func (p Secret) String() string {
+func (s Secret) String() string {
 	return "****"
 }
 
+func (s Secret) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.String())
+}
 
