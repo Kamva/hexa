74fd5a35e2c79190778c7fcf892c81f119264e0c
Mehran Prs <mehr.prs@gmail.com>
2020-02-29 23:29:24 +0330

    update error codes

--- errors.go
+++ errors.go
@@ -11,10 +11,10 @@ import (
 
 // Error code description:
 // kt = kitty  (package or project name)
-// 0 = replies about entity adapter section (identify some part in application)
+// ett = replies about entity adapter section (identify some part in application)
 // E = Error (type of code : error|response|...)
 // 0 = error number zero (id of code in that part and type)
 
 var (
-	ErrInvalidID = NewError(http.StatusBadRequest, "kttmpl.2.e.0", "invalid_id", errors.New("id value is invalid"))
+	ErrInvalidID = NewError(http.StatusBadRequest, "kt.ett.e.0", "err_invalid_id", errors.New("id value is invalid"))
 )
