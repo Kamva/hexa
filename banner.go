4476e0899f125c053ab51ada05cd5cb75cbb4187
Mehran Prs <mehr.prs@gmail.com>
2020-03-06 14:11:53 +0330

    Add proudct to banner

--- banner.go
+++ banner.go
@@ -5,13 +5,13 @@ import "github.com/labstack/gommon/color"
 // PrintBanner print the app's banner
 func PrintBanner(banner, product, version, website string) {
 	if banner == "" {
-		printKittyBanner()
+		printKittyBanner(product)
 		return
 	}
 	c := color.New()
 	c.Printf(banner, c.Yellow(product), c.Red("v"+version), c.Blue(website))
 }
 
-func printKittyBanner() {
-	PrintBanner(banner, "", Version, "")
+func printKittyBanner(product string) {
+	PrintBanner(banner, product, Version, "")
 }
