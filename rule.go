fb99f480abfa2a9a48eed87634d0fcf7b06528ea
mehran-prs <mehr.prs@gmail.com>
2020-11-15 11:38:32 +0330

    Revert "Add bool rule"
    This reverts commit 13325f759487364da1facc67e567089c46eb552a.

--- rule.go
+++ rule.go
@@ -1,13 +1,8 @@
 package hexa
 
-import "github.com/kamva/tracer"
-
 type (
 	// Rule is a rule signature.
 	Rule func() error
-
-	// BoolRule is a rule signature that specifies the rule is ok or not.
-	BoolRule func() (bool, error)
 
 	// RuleChecker get a list of rules and check if a rule is broken,
 	// returns that rule error message.
@@ -21,19 +16,8 @@ type (
 func (rc RuleChecker) CheckRules(rules ...Rule) error {
 	for _, r := range rules {
 		if err := r(); err != nil {
-			return tracer.Trace(err)
+			return err
 		}
 	}
 	return nil
-}
-
-// CheckRules check rules and returns first broken rule's error and whether its ok or not.
-// otherwise returns true,nil.
-func (rc RuleChecker) CheckBoolRules(rules ...BoolRule) (bool, error) {
-	for _, r := range rules {
-		if ok, err := r(); err != nil || !ok {
-			return ok, tracer.Trace(err)
-		}
-	}
-	return true, nil
 }
