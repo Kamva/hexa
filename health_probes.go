fc6d773b7a182893a6405b372aa4bf5e90654ad9
mehran-prs <mehr.prs@gmail.com>
2020-12-28 16:00:51 +0330

    Add healthCheck service interface

---
+++ health_probes.go
@@ -0,0 +1,58 @@
+package hexa
+
+import "context"
+
+type ReadinessStatus string
+
+const (
+	StatusReady   ReadinessStatus = "READY"
+	StatusUnReady ReadinessStatus = "UNREADY"
+)
+
+type LivenessStatus string
+
+const (
+	StatusAlive = "alive"
+	StatusDead  = "dead"
+)
+
+type Health interface {
+	Identifier() string
+	LivenessStatus(ctx context.Context) LivenessStatus
+	ReadinessStatus(ctx context.Context) ReadinessStatus
+}
+
+func LivenessCheck(l ...Health) map[string]LivenessStatus {
+	r := make(map[string]LivenessStatus, len(l))
+	for _, health := range l {
+		r[health.Identifier()] = health.LivenessStatus(context.Background())
+	}
+	return r
+}
+
+func ReadinessCheck(l ...Health) map[string]ReadinessStatus {
+	// TODO: check using go routine
+	r := make(map[string]ReadinessStatus, len(l))
+	for _, health := range l {
+		r[health.Identifier()] = health.ReadinessStatus(context.Background())
+	}
+	return r
+}
+
+type HealthCheckResult struct {
+	Live  LivenessStatus  `json:"live"`
+	Ready ReadinessStatus `json:"ready"`
+}
+
+func HealthCheck(l ...Health) map[string]HealthCheckResult {
+	// TODO: check using go routines
+	r := make(map[string]HealthCheckResult, len(l))
+	for _, health := range l {
+		r[health.Identifier()] = HealthCheckResult{
+			Live:  health.LivenessStatus(context.Background()),
+			Ready: health.ReadinessStatus(context.Background()),
+		}
+	}
+
+	return r
+}
