3d89827439e9b3d7c5bf98ce3a785f113cb40230
mehran-prs <mehr.prs@gmail.com>
2021-09-20 02:07:01 +0430

    Add mongodb monitor

---
+++ db/mgmadapter/monitoring.go
@@ -0,0 +1,98 @@
+package mgmadapter
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/kamva/hexa"
+	"github.com/kamva/hexa/hlog"
+	"go.mongodb.org/mongo-driver/event"
+)
+
+// composedMonitor composes multiple MongoDB server monitors.
+type composedMonitor struct {
+	monitors []*event.CommandMonitor
+}
+
+func NewComposedMonitor(monitors ...*event.CommandMonitor) *event.CommandMonitor {
+	m := &composedMonitor{monitors: monitors}
+
+	return &event.CommandMonitor{
+		Started:   m.Started,
+		Succeeded: m.Succeeded,
+		Failed:    m.Failed,
+	}
+}
+
+func (m *composedMonitor) Started(c context.Context, e *event.CommandStartedEvent) {
+	for _, monitor := range m.monitors {
+		if monitor.Started != nil {
+			monitor.Started(c, e)
+		}
+	}
+}
+
+func (m *composedMonitor) Succeeded(c context.Context, e *event.CommandSucceededEvent) {
+	for _, monitor := range m.monitors {
+		if monitor.Succeeded != nil {
+			monitor.Succeeded(c, e)
+		}
+	}
+}
+
+func (m *composedMonitor) Failed(c context.Context, e *event.CommandFailedEvent) {
+	for _, monitor := range m.monitors {
+		if monitor.Failed != nil {
+			monitor.Failed(c, e)
+		}
+	}
+}
+
+// logMonitor logs every command and its reply, so be careful and
+// don't use it in production mode.
+type logMonitor struct {
+	l hexa.Logger
+}
+
+func NewLogMonitor(l hexa.Logger) *event.CommandMonitor {
+	m := &logMonitor{l: l}
+	return &event.CommandMonitor{
+		Started:   m.Started,
+		Succeeded: m.Succeeded,
+		Failed:    m.Failed,
+	}
+}
+
+func (m *logMonitor) Started(c context.Context, e *event.CommandStartedEvent) {
+	// We can check if context is hexa context, log some data from the hexa context too.
+
+	m.l.Debug("MongoDB command started",
+		hlog.String("command", e.Command.String()),
+		hlog.String("db", e.DatabaseName),
+		hlog.String("command_name", e.CommandName),
+		hlog.String("request_id", strconv.FormatInt(e.RequestID, 10)),
+		hlog.String("connection_id", e.ConnectionID),
+	)
+}
+
+func (m *logMonitor) Succeeded(c context.Context, e *event.CommandSucceededEvent) {
+	// We can check if context is hexa context, log some data from the hexa context too.
+
+	m.l.Debug("MongoDB command succeeded",
+		hlog.String("reply", e.Reply.String()),
+		hlog.String("command_name", e.CommandName),
+		hlog.String("request_id", strconv.FormatInt(e.RequestID, 10)),
+		hlog.String("connection_id", e.ConnectionID),
+	)
+}
+
+func (m *logMonitor) Failed(c context.Context, e *event.CommandFailedEvent) {
+	// We can check if context is hexa context, log some data from the hexa context too.
+
+	m.l.Debug("MongoDB command failed",
+		hlog.String("failure", e.Failure),
+		hlog.String("command_name", e.CommandName),
+		hlog.String("request_id", strconv.FormatInt(e.RequestID, 10)),
+		hlog.String("connection_id", e.ConnectionID),
+	)
+}
