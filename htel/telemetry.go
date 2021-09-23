5de801705d2cf5be735031781acf4df2d9f9f208
mehran-prs <mehr.prs@gmail.com>
2021-09-23 14:51:29 +0330

    Add tracerProvider wrapper

--- htel/telemetry.go
+++ htel/telemetry.go
@@ -3,10 +3,25 @@ package htel
 import (
 	"context"
 
+	"github.com/kamva/hexa"
 	"github.com/kamva/tracer"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 )
+
+// tracerProvider is a wrapper around the tracesdk.TracerProvider to be sure implements a hexa
+// service to shutdown the TracerProvider.
+type tracerProvider struct {
+	*tracesdk.TracerProvider
+}
+
+func (t *tracerProvider) Shutdown(ctx context.Context) error {
+	return tracer.Trace(t.TracerProvider.Shutdown(ctx))
+}
+
+func NewTracerProvider(tp *tracesdk.TracerProvider) *tracerProvider {
+	return &tracerProvider{tp}
+}
 
 // OpenTelemetry is just a wrapper for openTelemetry services to
 // implement hexa services(to shutdown... them).
@@ -14,19 +29,16 @@ type OpenTelemetry interface {
 	TracerProvider() trace.TracerProvider
 }
 
-func NewOpenTelemetry(tp *tracesdk.TracerProvider) OpenTelemetry {
+func NewOpenTelemetry(tp trace.TracerProvider) OpenTelemetry {
 	return &openTelemetry{tp: tp}
 }
 
 type openTelemetry struct {
-	tp *tracesdk.TracerProvider
+	tp trace.TracerProvider
 }
 
 func (t *openTelemetry) TracerProvider() trace.TracerProvider {
 	return t.tp
 }
 
-func (t *openTelemetry) Shutdown(c context.Context) error {
-	// Shutdown other open telemetry instances here.
-	return tracer.Trace(t.tp.Shutdown(c))
-}
+var _ hexa.Shutdownable = &tracerProvider{}
