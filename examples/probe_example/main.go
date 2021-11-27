445c88ae4cc1c45804a1e6eef30ee1037ae9a65c
mehran-prs <mehr.prs@gmail.com>
2021-11-27 16:14:12 +0330

    Add probe server

---
+++ examples/probe_example/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"syscall"
+
+	"github.com/kamva/gutil"
+	"github.com/kamva/hexa/probe"
+)
+
+const addr = "localhost:7676"
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	probeServer := probe.NewProbeServer(server, mux)
+
+	probeServer.Register("hi", "/hi", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("hi"))
+		if err != nil {
+			panic(err)
+		}
+	}, "hi server")
+
+	gutil.PanicErr(probeServer.Run())
+	fmt.Println("server is listening on", addr)
+	gutil.WaitForSignals(syscall.SIGINT)
+}
