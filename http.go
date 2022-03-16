bb522f1c292c62fb414ba453521eea80bdabbea8
mehran-prs <mehr.prs@gmail.com>
2022-03-16 16:19:36 +0330

    Update HttpResponseBody

--- http.go
+++ http.go
@@ -5,23 +5,8 @@ package hexa
 // HttpRespBody is the http response body format
 //easyjson:json
 type HttpRespBody struct {
-	Code    string       `json:"code"`
-	Message string       `json:"message,omitempty"`
-	Data    interface{}  `json:"data,omitempty"`
-	Debug   interface{}  `json:"debug,omitempty"` // Set this value to nil when you are on production mode.
-}
-
-// SetDebug set debug flag and debug data.
-func (b HttpRespBody) SetDebug(debugData interface{}) HttpRespBody {
-	b.Debug = debugData
-	return b
-}
-
-// NewBody return new instance of the HttpRespBody
-func NewBody(code string, msg string, data interface{}) HttpRespBody {
-	return HttpRespBody{
-		Code:    code,
-		Message: msg,
-		Data:    data,
-	}
+	Code    string      `json:"code"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	Debug   interface{} `json:"debug,omitempty"` // Set this value to nil when you are on production mode.
 }
