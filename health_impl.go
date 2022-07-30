7222286bd4b3724ff183aec0cead6e5e97c8a35b
mehran-prs <mehr.prs@gmail.com>
2022-07-31 01:04:33 +0430

    Implement ping health

---
+++ health_impl.go
@@ -0,0 +1,60 @@
+package hexa
+
+import (
+	"context"
+)
+
+type Ping func(ctx context.Context) error
+
+type pingHealth struct {
+	l          Logger
+	identifier string
+	ping       Ping
+	tags       map[string]string
+}
+
+func NewPingHealth(l Logger, identifier string, ping Ping, tags map[string]string) Health {
+	return &pingHealth{
+		identifier: identifier,
+		ping:       ping,
+		tags:       tags,
+	}
+}
+
+func (h *pingHealth) HealthIdentifier() string {
+	return h.identifier
+}
+
+func (h *pingHealth) LivenessStatus(ctx context.Context) LivenessStatus {
+	if err := h.ping(ctx); err != nil {
+		h.l.Error("can not ping", StringField("health_identifier", h.identifier), ErrField(err))
+		return StatusDead
+	}
+
+	return StatusAlive
+}
+
+func (h *pingHealth) ReadinessStatus(ctx context.Context) ReadinessStatus {
+	if h.LivenessStatus(ctx) == StatusAlive {
+		return StatusReady
+	}
+
+	return StatusUnReady
+}
+
+func (h *pingHealth) HealthStatus(ctx context.Context) HealthStatus {
+	liveness := h.LivenessStatus(ctx)
+	readiness := StatusReady
+	if liveness == StatusDead {
+		readiness = StatusUnReady
+	}
+
+	return HealthStatus{
+		Id:    h.HealthIdentifier(),
+		Alive: liveness,
+		Ready: readiness,
+		Tags:  h.tags,
+	}
+}
+
+var _ Health = &pingHealth{}
