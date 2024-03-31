74c267de1acfd0eb96e78fe0598a3e9d29126af4
Mehdi Meshkatian <mmeshkatian@gmail.com>
2024-03-31 14:32:20 +0330

    Add body in hurl errors

--- hurl/error.go
+++ hurl/error.go
@@ -9,16 +9,20 @@ import (
 type HTTPErr struct {
 	Code   int
 	Status string
+	Body string
 }
 
 func (err HTTPErr) Error() string {
-	return fmt.Sprintf("HTTP code: %d, msg: %s, status: %s", err.Code, http.StatusText(err.Code), err.Status)
+	return fmt.Sprintf("HTTP code: %d, msg: %s, status: %s, body: %s", err.Code, http.StatusText(err.Code), err.Status, err.Body)
 }
 
 // ResponseErr returns http error if the response is not successful.
-func ResponseErr(r *http.Response) *HTTPErr {
+func responseErr(r *http.Response) error {
 	if r.StatusCode <= 300 {
 		return nil
 	}
-	return &HTTPErr{r.StatusCode, r.Status}
+
+	body, _ := io.ReadAll(r.Body)
+
+	return HttpErr{r.StatusCode, r.Status, string(body)}
 }
